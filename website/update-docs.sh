13b298d1d5d15575cb44b7573bef230b49abdd33
James O'Gorman <james@netinertia.co.uk>
2015-04-04 17:34:28 +0000

    Update website docs script.
    The docs location was reorganised 6 years ago(!).
    Fix the rsync call too. It didn't do anything as it was.

--- website/update-docs.sh
+++ website/update-docs.sh
@@ -3,6 +3,6 @@
 export PATH=$PATH:/usr/local/bin
 cd ~/trunk
 svn update
-cd documentation/boxbackup
+cd docs
 make clean docs
-/usr/local/bin/rsync box-html/ /usr/local/www/htdocs
+/usr/local/bin/rsync -rv htmlguide/ /usr/local/www/htdocs
